c997fce23eff2f7d0d3a5e80792ae6bbc69e116e
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2022-01-06 15:40:24 +0200

    new exercise

---
+++ AnimalsClass/AnimalsClass/Animal.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsClass
+{
+    public class Animal
+    {
+        private string name;
+        public string Name { get => name; set => name = value; }
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    public abstract class Mammal : Animal
+    {
+        public Mammal(string name) : base(name)
+        {
+            Console.WriteLine("I am mammal.");
+        }
+        public abstract int GetSpeed();
+    }
+
+    public abstract class Fish : Animal
+    {
+        public Fish(string name) : base(name)
+        {
+            Console.WriteLine("I am fish.");
+        }
+        public abstract int GetSpeed();
+    }
+
+    public abstract class Bird : Animal
+    {
+        public Bird(string name) : base(name)
+        {
+            Console.WriteLine("I am bird.");
+        }
+        public abstract int GetSpeed();
+    }
+    public abstract class Reptile : Animal
+    {
+        public Reptile(string name) : base(name)
+        {
+            Console.WriteLine("I am reptile.");
+        }
+        public abstract int GetSpeed();
+    }
+    public abstract class Insect : Animal
+    {
+        public Insect(string name) : base(name)
+        {
+            Console.WriteLine("I am Insect.");
+        }
+        public abstract int GetSpeed();
+    }
+
+    public class Platypus : Mammal, ISwim, IWalk
+    {
+        public Platypus(string name) : base(name) { }
+        public void Swim()
+        {
+            Console.WriteLine("I am a Platypus and I can swim");
+        }
+
+        public override int GetSpeed()
+        {
+            return 30;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("I am a Platypus and I can walk");
+        }
+    }
+
+    public class WildDuck : Bird, ISwim, IFly, IWalk
+    {
+        public WildDuck(string name) : base(name) { }
+        public void Swim()
+        {
+            Console.WriteLine("I am a WildDuck and I can swim");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("I am a WildDuck and I can fly");
+        }
+
+        public override int GetSpeed()
+        {
+            return 40;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("I am a WildDuck and I can walk");
+        }
+    }
+
+    public class Bat : Mammal, IFly
+    {
+        public Bat(string name) : base(name) { }
+        public void Fly()
+        {
+            Console.WriteLine("I am a Bat and I can fly");
+        }
+
+        public override int GetSpeed()
+        {
+            return 20;
+        }
+    }
+
+    public class Ostrich : Bird, IWalk
+    {
+        public Ostrich(string name) : base(name) { }
+        public void Walk()
+        {
+            Console.WriteLine("I am a ostrich and I can walk");
+        }
+
+        public override int GetSpeed()
+        {
+            return 10;
+        }
+    }
+}
---
+++ AnimalsClass/AnimalsClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AnimalsClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Platypus lulu = new Platypus("Lulu");
+            lulu.Swim();
+            Console.WriteLine("My name is {0} and I can have a speed of {1} km/hour.", lulu.Name, lulu.GetSpeed()); 
+        }
+    }
+
+    interface IFly
+    {
+        void Fly();
+    }
+    interface ISwim
+    {
+        void Swim();
+    }
+    interface IWalk
+    {
+        void Walk();
+    }
+    interface ICrowl
+    {
+        void Crowl();
+    }
+}
+
+/*
+Make an application where to model animals, keeping in mind that:
+any kind you model is an animal
+any animal has a name
+
+there are multiple animal categories like : mammals, fish, birds, reptile, insects
+no matter the category, animal are : flying, walking, swim or crawl.
+depending on the way they move, they have a maximum speed.
+
+there are animals that can swim and walk (Platypus)
+there are animals that can swim, fly and walk (Wild Duck)
+Not any animal that can fly is a bird, the bat is a mammal that flys, 
+and not evey bird flys - the ostrich is a bird but doesn't fly
+*/
