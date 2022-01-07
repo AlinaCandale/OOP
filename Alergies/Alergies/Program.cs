9a4f6282a0e35dce843b0253d67f8be24953fb8e
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2022-01-07 14:47:51 +0200

    Allergies exercise

---
+++ Alergies/Alergies/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alergies
+{
+    enum Allergens
+    {
+        Eggs = 1,
+        Peanuts = 2,
+        Shellfish = 4,
+        Strawberries = 8,
+        Tomatoes = 16,
+        Chocolate = 32,
+        Pollen = 64,
+        Cats = 128
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Allergies alergy = new Allergies(34);
+            alergy.IsAllergic("Peanuts");
+            alergy.IsAllergic("Eggs");
+            alergy.PrintAllAllergies();
+
+            Allergies alergy2 = new Allergies(257);
+            alergy2.IsAllergic("Peanuts");
+            alergy2.IsAllergic("Eggs");
+            alergy2.PrintAllAllergies();
+        }
+    }
+
+    class Allergies
+    {
+        List<string> allergyList = new List<string>();
+
+        public void IsAllergic(string allergen)
+        {
+            if (allergyList.Contains(allergen))
+            {
+                Console.WriteLine($"You are allergic at {allergen}");
+            }
+            else
+            {
+                Console.WriteLine($"You are not allergic at {allergen}");
+            }
+        }
+
+        public Allergies(int score)
+        {
+            foreach (Allergens item in Enum.GetValues(typeof(Allergens)))
+            {
+                if (((int)item & score) != 0)
+                {
+                    allergyList.Add(item.ToString());
+                }
+            }
+        }
+
+        public void PrintAllAllergies()
+        {
+            Console.WriteLine("All the allergens you are allergic to:");
+            foreach (var item in allergyList)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+Given a person's allergy score, determine whether or not they're allergic to a given item, and their full list of allergies.
+An allergy test produces a single numeric score which contains the information about all the allergies the person has (that they were tested for).
+The list of items (and their value) that were tested are:
+eggs (1)
+peanuts (2)
+shellfish (4)
+strawberries (8)
+tomatoes (16)
+chocolate (32)
+pollen (64)
+cats (128)
+So if Tom is allergic to peanuts and chocolate, he gets a score of 34 (2 - from peanuts + 32 from chocholate)
+Now, given just that score of 34, your program should be able to say:
+Whether Tom is allergic to any one of those allergens listed above.
+All the allergens Tom is allergic to.
+Note: a given score may include allergens not listed above (i.e. allergens that score 256, 512, 1024, etc.). Your program should ignore those components of the score.
+For example, if the allergy score is 257, your program should only report the eggs (1) allergy.
+*/
